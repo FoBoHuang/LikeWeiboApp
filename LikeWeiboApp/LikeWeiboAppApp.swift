7b30ab2498f8612daff3fdecf8b97107577f741a
fob <hfb161716@163.com>
2025-01-13 23:50:54 +0800

    Initial Commit

---
+++ LikeWeiboApp/LikeWeiboAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LikeWeiboAppApp.swift
+//  LikeWeiboApp
+//
+//  Created by 黄佛波 on 2025/1/13.
+//
+
+import SwiftUI
+
+@main
+struct LikeWeiboAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LikeWeiboAppTests/LikeWeiboAppTests.swift
@@ -0,0 +1,16 @@
+//
+//  LikeWeiboAppTests.swift
+//  LikeWeiboAppTests
+//
+//  Created by 黄佛波 on 2025/1/13.
+//
+
+import Testing
+
+struct LikeWeiboAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
