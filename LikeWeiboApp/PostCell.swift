6469503c7b733f3fc426170d7b2dc9c8bb265b25
fob <hfb161716@163.com>
2025-01-18 18:06:47 +0800

    add

--- LikeWeiboApp/PostCell.swift
+++ LikeWeiboApp/PostCell.swift
@@ -9,7 +9,43 @@ import SwiftUI
 
 struct PostCell: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        HStack(spacing: 5) {
+            Image(uiImage: UIImage(named:"005tnxzUly8gab4i2r73xj30u00u0js8.jpg")!)
+                .resizable()
+                .scaledToFit()
+                .frame(width: 50, height: 50)
+                .clipShape(Circle())
+                .overlay {
+                        PostVIPBadge()
+                        .offset(x: 16, y: 16)
+                }
+            
+            VStack(alignment: .leading, spacing: 5) {
+                Text("用户昵称")
+                    .font(Font.system(size: 16, weight: Font.Weight.black))
+                    .foregroundColor(Color.red)
+                    .lineLimit(1)
+                Text("2025-01-18")
+                    .font(.system(size: 16))
+                    .foregroundColor(.gray)
+            }
+            .padding(.leading, 10)
+            
+            Spacer()
+            
+            Button(action: {
+                print("Click follow me")
+            }) {
+                Text("关注")
+                    .font(.system(size: 14))
+                    .foregroundColor(.orange)
+                    .frame(width: 50, height: 26)
+                    .overlay(content: {
+                        RoundedRectangle(cornerRadius: 13) // 圆角矩形
+                            .stroke(Color.orange, lineWidth: 1) // 描边
+                    })
+            }
+        }
     }
 }
 
--- LikeWeiboApp/PostVIPBadge.swift
+++ LikeWeiboApp/PostVIPBadge.swift
@@ -9,7 +9,17 @@ import SwiftUI
 
 struct PostVIPBadge: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text("V")
+            .bold()
+            .font(.system(size: 11))
+            .frame(width: 15, height: 15)
+            .foregroundColor(Color.yellow)
+            .background(Color.red)
+            .clipShape(Circle())
+            .overlay {
+                RoundedRectangle(cornerRadius: 7.5)
+                    .stroke(Color.white, lineWidth: 1)
+            }
     }
 }
 
