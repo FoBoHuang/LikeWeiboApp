f2f86e86efc8298d50a19dc81a66bee5bb0e6978
fob <hfb161716@163.com>
2025-01-18 18:05:59 +0800

    add

---
+++ LikeWeiboApp/PostCell.swift
@@ -0,0 +1,18 @@
+//
+//  PostCell.swift
+//  LikeWeiboApp
+//
+//  Created by 黄佛波 on 2025/1/18.
+//
+
+import SwiftUI
+
+struct PostCell: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PostCell()
+}
---
+++ LikeWeiboApp/PostVIPBadge.swift
@@ -0,0 +1,18 @@
+//
+//  PostVIPBadge.swift
+//  LikeWeiboApp
+//
+//  Created by 黄佛波 on 2025/1/18.
+//
+
+import SwiftUI
+
+struct PostVIPBadge: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PostVIPBadge()
+}
