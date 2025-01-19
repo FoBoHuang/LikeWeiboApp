8398df9b2ce007ca609f819116b1c9e3a4b166a5
fob <hfb161716@163.com>
2025-01-19 23:44:36 +0800

    add 评论和点赞

---
+++ LikeWeiboApp/PostCellToolbarButton.swift
@@ -0,0 +1,18 @@
+//
+//  PostCellToolbarButton.swift
+//  LikeWeiboApp
+//
+//  Created by 黄佛波 on 2025/1/19.
+//
+
+import SwiftUI
+
+struct PostCellToolbarButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PostCellToolbarButton()
+}
