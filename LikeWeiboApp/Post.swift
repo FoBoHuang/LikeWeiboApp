e5a2013ca8b1d7a2f03ee14788b675878ac5e2e9
fob <hfb161716@163.com>
2025-01-19 18:26:05 +0800

    add post list

---
+++ LikeWeiboApp/Post.swift
@@ -0,0 +1,18 @@
+//
+//  Post.swift
+//  LikeWeiboApp
+//
+//  Created by 黄佛波 on 2025/1/19.
+//
+
+import SwiftUI
+
+struct Post: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Post()
+}
